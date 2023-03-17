47b071b8e59fc1fd54428fc6e9a4c37828b6d623
Jayesh bellani <54457951+jbellani99@users.noreply.github.com>
2023-03-17 16:26:13 +0530

    completed

---
+++ practical5/practical5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class MyClass
+{
+
+    public int[] arr =new int[] {1,2,3,4,5};
+    public static void Main() {
+       
+
+        MyClass obj=new MyClass();
+        try
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+
+                Console.WriteLine($"{i}:{obj.arr[i]}");
+
+            }
+
+
+
+        }
+        catch (IndexOutOfRangeException e)
+        {
+
+
+            Console.WriteLine(e.Message);
+          
+
+        }
+        catch (Exception ex) {
+
+            Console.WriteLine(ex.Message);
+        
+        }
+        finally { 
+            Console.WriteLine("Finally Block");
+        }
+    }
+}
